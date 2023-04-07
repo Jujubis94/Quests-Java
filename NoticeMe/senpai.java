d96d19e0a8f5c35a1cab6e76fb74d0d5ffaf01b8
Julien Marcenac <julien.marcenac2@gmail.com>
2023-04-07 17:16:04 +0200

    done

---
+++ NoticeMe/senpai.java
@@ -0,0 +1,7 @@
+package NoticeMe;
+class Senpai {
+    public static void main(String[] args) {
+        // afficher Hello World! dans le terminal
+        System.out.println("Notice me Senpai");
+    }
+}
