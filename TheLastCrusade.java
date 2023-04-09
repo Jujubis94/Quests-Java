ef3d56a874bd4fd2b437ad4e23832d765621ddf4
Julien Marcenac <julien.marcenac2@gmail.com>
2023-04-09 15:24:42 +0200

    done

---
+++ TheLastCrusade.java
@@ -0,0 +1,10 @@
+class TheLastCrusade {
+    public static void main(String[] args) {
+        String title = "Indiana Jones and the Last Crusade";
+        boolean watched = false;
+        int releaseYear = 1989;
+        float note = (float) 8.2;
+
+        System.out.println("titre: " + title + " vu: " + watched + " année: " + releaseYear + " note: " + note);
+    }
+}
